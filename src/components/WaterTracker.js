fa126bc8f41e79b0e504a0ef88ac182fd1e1a682
Kapil Lamba <166989106+kapillamba08@users.noreply.github.com>
2025-07-16 09:15:55 +0530

    react native project

---
+++ src/components/WaterTracker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WaterTracker = () => (
+  <View>
+    <Text>Water Tracker Component</Text>
+  </View>
+);
+
+export default WaterTracker;
